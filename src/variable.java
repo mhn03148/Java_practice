3c5e87d19febe9379d11cb460a44d5a61e306894
jujinhyeong <mhn03148@naver.com>
2022-11-12 23:52:00 +0900

    영문자, 숫자, _, 한글 조합으로 변수명을 짓는다. Ex) thisYear, man1, man2, 변수 반드시 첫글자는 영문자 선호한다. 예약어(Reserved Word)를 변수명으로 사용할 수 없다. Ex) in, if, double 대소문자를 구분해서 사용한다. 변수명은 128자까지 인식한다.

--- src/variable.java
+++ src/variable.java
@@ -1,5 +1,12 @@
 public class variable {
-    public static void main (String[] args){
-        System.out.println("hello");
+    public static void main (String[] args) {
+        int age = 27;
+        System.out.println(age + "입니다.");
+        //비교연산자를 이용한 예시
+        boolean adult = age > 19;
+        if (adult == true)
+            System.out.println(age + "살은 성인");
+        else
+            System.out.println(age + "살은 미성년자");
     }
 }

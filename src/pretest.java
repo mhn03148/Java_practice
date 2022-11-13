ff7e84e58d16edf7974c290fd3bc2140ed372b48
jujinhyeong <mhn03148@naver.com>
2022-11-14 02:15:54 +0900

    현대 pretest

---
+++ src/pretest.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lines = Integer.parseInt(br.readLine());
+        int[][] area = new int[lines][lines];
+        for(int i=0; i<lines; i++){
+            int arr = Integer.parseInt(br.readLine());
+        }
+        System.out.println(area);
+        System.out.println(lines);
+    }
+}
